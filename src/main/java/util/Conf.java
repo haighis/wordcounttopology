8b6c0fb209f628c1c7e71c58f013e9a1e07aa8d2
John Haigh <haighis@gmail.com>
2014-01-15 06:50:02 -0500

    updated

--- src/main/java/util/Conf.java
+++ src/main/java/util/Conf.java
@@ -8,8 +8,7 @@ public class Conf {
     public static final String REDIS_HOST_KEY = "redisHost";
     public static final String REDIS_PORT_KEY = "redisPort";
     public static final String DEFAULT_JEDIS_PORT = "6379";
-    public static final String AZURE_QUEUE_POPBOOKMARK = "http://rtspappsvc1.azurewebsites.net/actions/appservice/popbookmark";
-    
+ 
     public static byte[] GetWordCountUrlsQueueName()
     {
     	byte[] ar = null;
